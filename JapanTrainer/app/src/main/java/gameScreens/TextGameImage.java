2f0a7d37b00268b7926bf8698ee2bc886068ea7d
Dumbyarnik <daniel.vorobyev@zoho.eu>
2020-08-12 18:52:03 +0200

    Created Screen for TextGameImage

---
+++ JapanTrainer/app/src/main/java/gameScreens/TextGameImage.java
@@ -0,0 +1,13 @@
+package gameScreens;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TextGameImage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_text_image);
+    }
+}
