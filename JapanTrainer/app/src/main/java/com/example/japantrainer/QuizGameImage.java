756262d4a13e2112ddbe699ebc3a9a360c8067e8
Dumbyarnik <daniel.vorobyev@zoho.eu>
2020-08-12 23:40:35 +0200

    Created QuizGameImage files

---
+++ JapanTrainer/app/src/main/java/com/example/japantrainer/QuizGameImage.java
@@ -0,0 +1,13 @@
+package com.example.japantrainer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuizGameImage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_quiz_image);
+    }
+}
