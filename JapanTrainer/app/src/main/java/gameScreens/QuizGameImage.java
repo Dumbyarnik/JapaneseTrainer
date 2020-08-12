0f80b5350d81e553147aaa362762d804d6e7ae81
Dumbyarnik <daniel.vorobyev@zoho.eu>
2020-08-13 01:40:49 +0200

    New Score Screen

--- JapanTrainer/app/src/main/java/gameScreens/QuizGameImage.java
+++ JapanTrainer/app/src/main/java/gameScreens/QuizGameImage.java
@@ -65,7 +65,11 @@ public class QuizGameImage extends AppCompatActivity {
         Bitmap bitmap = BitmapFactory.decodeByteArray(image, 0, image.length);
         imageView.setImageBitmap(bitmap);
 
+<<<<<<< Updated upstream
         //Setting text to buttons
+=======
+        // Setting text to buttons
+>>>>>>> Stashed changes
         Button btn1 = findViewById(R.id.btn1);
         Button btn2 = findViewById(R.id.btn2);
         Button btn3 = findViewById(R.id.btn3);
---
+++ JapanTrainer/app/src/main/java/gameScreens/ScoreScreen.java
@@ -0,0 +1,16 @@
+package gameScreens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.japantrainer.R;
+
+public class ScoreScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.screen_score);
+    }
+}
