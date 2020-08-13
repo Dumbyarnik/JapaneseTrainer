be3fdb26a6ca8c3ef363578e817f7ec35d25912d
Dumbyarnik <daniel.vorobyev@zoho.eu>
2020-08-13 23:34:01 +0200

    Created Score Screen

--- JapanTrainer/app/src/main/java/gameScreens/ScoreScreen.java
+++ JapanTrainer/app/src/main/java/gameScreens/ScoreScreen.java
@@ -1,16 +1,53 @@
-package gameScreens;
+package startScreens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.japantrainer.R;
 
+import helpClasses.managerClasses.PointsManager;
+import startScreens.HomeScreen;
+
 public class ScoreScreen extends AppCompatActivity {
+
+    private PointsManager pointsManager;
+
+    // UI Variables
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.screen_score);
+
+        // Initialization
+        pointsManager = new PointsManager(this);
+
+        // Setting points from to TextView
+        textView = findViewById(R.id.points);
+        textView.setText(String.valueOf(pointsManager.getPoints()));
+
+        // Setting button
+        Button btn = findViewById(R.id.next);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openHomeScreen();
+            }
+        });
+    }
+
+    // Opens next Screens after pressing a button
+    private void openHomeScreen()   {
+
+        // Going to the next screen
+        Intent intent = new Intent(this, HomeScreen.class);
+        startActivity(intent);
     }
 }
